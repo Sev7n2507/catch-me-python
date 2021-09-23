ae3bb7897d110d1d09259eefc9b0b67dd8313b6d
Sofian Slimani <sofianslimani48@gmail.com>
2021-09-23 23:43:21 +0200

    test for pause

--- python.py
+++ python.py
@@ -11,8 +11,8 @@ ecran = pygame.display.set_mode((800,800))
 
 pygame.display.set_caption("Catch me 3000")
 
-background = pygame.image.load('./Python/assets/background.jpg')
-image = pygame.image.load("./Python/assets/catch-me.png")
+background = pygame.image.load('./catch-me-python/assets/background.jpg')
+image = pygame.image.load("./catch-me-python/assets/catch-me.png")
 pygame.display.set_icon(image)
 print(image)
 
@@ -20,14 +20,18 @@ loop = True
 p = Player(ecran)
 e = Enemy(ecran,p)
 clock = pygame.time.Clock()
-laps = 0
 font = pygame.font.Font(None, 54)
 
 
 while loop:
     
+    
+    
     ecran.blit(background, (0,0))
-
+    e.draw()
+    p.draw()    
+    
+    
     for event in pygame.event.get():
         #event input clavier
         if event.type == pygame.KEYDOWN:
@@ -43,29 +47,39 @@ while loop:
                 p.deplacement(0, 1)
         if event.type == pygame.QUIT:
             loop = False
-    e.draw()
-    p.draw()    
+            
+    pygame.display.flip()        
+    
     if e.follow() :
         
+  
+       
+            
         #gameover = font.render("Press R to Respawn", False, (255, 255, 255))
         #rect = gameover.get_rect()
         #rect.center = ecran.get_rect().center
         #ecran.blit(gameover, rect)
-      
-        restart = input('do you want to restart Y/N?')
-        if restart == 'N':
-            break
-        elif restart == 'Y':
-           
-            continue
+        
+
+        ecran.blit(background, (0,0))
+        e.draw()
+        p.draw() 
+        pygame.display.flip()
+        pygame.display.update()
+        clock.tick(30)
+        
+        #restart = input('do you want to restart Y/N?')
+        #if restart == 'N':
+        #    break
+        #elif restart == 'Y':
+        
+        #    continue
+     
         
     
     
-       
     
     
-    
-    pygame.display.flip()
     
        
 pygame.quit()
